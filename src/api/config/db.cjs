04f8dc2188071e70d908bfa4850f054df8de701c
Damien <tacitedamien444@gmail.com>
2024-09-05 11:29:37 +0200

    patch

--- src/api/config/db.cjs
+++ src/api/config/db.cjs
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-dotenv.config();
+
+dotenv.config({ path: '.env.local' });
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI); 
     console.log('MongoDB a été connecté avec succès');
   } catch (err) {
     console.error('MongoDB : erreur de connexion:', err);
